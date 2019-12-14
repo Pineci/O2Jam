70c86746da18bffb695012922d7e5ffea8b74b52
Pineci <36169731+Pineci@users.noreply.github.com>
2019-12-14 02:56:38 -0800

    Initial Commit
    Added different versions of O2Jam in the flow vs choke project

---
+++ Anthony/open2jam-Alpha_7/src/org/open2jam/game/judgment/JudgmentStrategy.java
@@ -0,0 +1,53 @@
+package org.open2jam.game.judgment;
+
+import org.open2jam.render.entities.NoteEntity;
+import org.open2jam.game.TimingData;
+
+/**
+ * A hit judgment strategy.
+ * @author dttvb
+ */
+public interface JudgmentStrategy {
+    
+    /**
+     * Returns true if the note should be accepted (judged) by the game.
+     * 
+     * @param note the note to check
+     * @return true if the note is to be judged
+     */
+    boolean accept(NoteEntity note);
+    
+    /**
+     * Returns true if the player missed the note.
+     * 
+     * @param note the note to check
+     * @return true if the note is missed, false otherwise
+     */
+    boolean missed(NoteEntity note);
+    
+    /**
+     * Sets the timing data associated with this judge.
+     * 
+     * @param timing timing data to set
+     */
+    void setTiming(TimingData timing);
+
+    /**
+     * Judge the note.
+     * 
+     * @param note the note to judge
+     * @return the result of the judgment
+     */
+    JudgmentResult judge(NoteEntity note);
+    
+    public void decideRandomMiss();
+    
+    /**
+     * Must be called after decideRandomMiss, returns true if the note
+     * is to be judged normally, returns false if the note will be forced
+     * to be a miss.
+     * @return the random miss result
+     */
+    public boolean passRandomThreshold();
+    
+}
